f5285c6bbecb124ab91bcf407096e70ee2620987
bharding2 <b.harding@gmail.com>
2020-06-05 15:24:29 -0700

    day 1 code cleanup

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ function Book(title, author, pageCount) {
   this.createBook = function(parentElement) {
     var bookElement = document.createElement('div');
     bookElement.classList.add('book');
-    
+
     var titleElement = document.createElement('div');
     titleElement.classList.add('title');
     titleElement.textContent = this.title;
-    
+
     var authorElement = document.createElement('div');
     authorElement.classList.add('author');
     authorElement.textContent = this.author;
-    
+
     bookElement.appendChild(titleElement);
     bookElement.appendChild(authorElement);
-    
+
     parentElement.appendChild(bookElement);
   };
-}
+};
 
 var book1 = new Book('first book', 'Noah', 1000);
 console.log(book1);
@@ -36,18 +36,18 @@ var book2 = {
 function createBook(book, parentElement) {
   var bookElement = document.createElement('div');
   bookElement.classList.add('book');
-  
+
   var titleElement = document.createElement('div');
   titleElement.classList.add('title');
   titleElement.textContent = book.title;
-  
+
   var authorElement = document.createElement('div');
   authorElement.classList.add('author');
   authorElement.textContent = book.author;
-  
+
   bookElement.appendChild(titleElement);
   bookElement.appendChild(authorElement);
-  
+
   parentElement.appendChild(bookElement);
 }
 
